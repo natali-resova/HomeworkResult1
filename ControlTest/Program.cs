ce5f0c3e37083031b1ad1f205df8ae721875f98b
Natali Tremasova <n.resova@yandex.ru>
2024-02-25 22:19:18 +0300

    add method: GetNumberOfElements

--- ControlTest/Program.cs
+++ ControlTest/Program.cs
@@ -14,3 +14,19 @@ int GetNumberOfChar(string arr)
 //string arr = "Hellomfktio";
 //int b = GetNumberOfChar(arr);
 // Console.Write(b);
+
+//2. Определение кол-ва элементов массива, соответствующих заданному условию
+
+int GetNumberOfElements(string[] array)
+{
+    int count = 0;//счетчик элементов массива, соответствующих условию(<= 3)
+    for (int j = 0; j < array.Length; j++)
+    {        
+        int d = GetNumberOfChar(array[j]);
+        if (d <= 3) count++;       
+    }      
+    return count;
+    
+}
+int b = GetNumberOfElements(array);
+//Console.Write(b);
