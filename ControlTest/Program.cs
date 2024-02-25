7c65df2e7ef0a7f35627a169393ffb03ebb7da04
Natali Tremasova <n.resova@yandex.ru>
2024-02-25 21:56:40 +0300

    add method: GetNumberOfChar

--- ControlTest/Program.cs
+++ ControlTest/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1. Определение количества символов в элементе массива строкового типа
+int GetNumberOfChar(string arr)
+{ 
+ string str = " ";
+        int k = 0; //счетчик кол-ва символов в строке
+        foreach (char e in arr)
+        {
+            str += e;
+            k++;
+        }
+        return k;
+}
+
+//string arr = "Hellomfktio";
+//int b = GetNumberOfChar(arr);
+// Console.Write(b);
